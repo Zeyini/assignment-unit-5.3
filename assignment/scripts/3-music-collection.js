aef29fd9b6baf023e702cf71b9b126eff27989c8
Zeyini Mohammad <Zeyinim@outlook.com>
2024-03-23 02:20:36 -0500

    created myCollection with an empty array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,9 +1,34 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
+// Create an empty array named myCollection
+let myCollection = [];
 
+// Function to add an album to the collection
+function addToCollection(collection, title, artist, yearPublished) {
+    // Create a new album object
+    let newAlbum = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    };
+    
+    // Add the new album to the collection
+    collection.push(newAlbum);
+    // Return the newly created object
+    return newAlbum;
+}
 
-
+// Log the return value / Add albums to the collection
+console.log(addToCollection(myCollection, "Album 1", "Artist A", 2001));
+console.log(addToCollection(myCollection, "Album 3", "Artist B", 2003));
+console.log(addToCollection(myCollection, "Album 3", "Artist A", 2015));
+console.log(addToCollection(myCollection, "Album 4", "Artist C", 2005));
+console.log(addToCollection(myCollection, "Album 5", "Artist B", 2020));
+console.log(addToCollection(myCollection, "Album 6", "Artist A", 2012));
+console.log(addToCollection(myCollection, "Album 6", "Artist A", 2012));
+// log myCollection
+console.log(myCollection);
 
 
 
